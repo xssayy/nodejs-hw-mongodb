18e74d51d96afd85fd552768b2b98a2b450e0cf4
Mykyta Kudlai <nikiatanik@gmail.com>
2024-06-19 19:03:20 +0200

    upd

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,9 +4,9 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   phoneNumber: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.valid('work', 'home', 'personal').required(),
+  contactType: Joi.valid('work', 'home', 'personal'),
 });
 
 export const updateContactSchema = Joi.object({
