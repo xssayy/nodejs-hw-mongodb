93241a8be6b0afb9d3bfcaf8d97b34ae0334d23e
Mykyta Kudlai <nikiatanik@gmail.com>
2024-07-14 04:33:04 +0200

    created API-DOCS

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,6 +19,8 @@ export const CLOUDINARY = {
   CLOUD_API_SECRET: 'CLOUD_API_SECRET',
 };
 
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
+
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
--- src/routers/index.js
+++ src/routers/index.js
@@ -3,11 +3,12 @@ import { Router } from 'express';
 
 import usersRouter from './contacts.js';
 import authRouter from './auth.js';
-import { authenticate } from '../middlewares/authenticate.js';
+import { swaggerDocs } from '../middlewares/swaggerDocs.js';
 
 const router = Router();
 
 router.use('/contacts', usersRouter);
 router.use('/auth', authRouter);
+router.use('/api-docs', swaggerDocs());
 
 export default router;
